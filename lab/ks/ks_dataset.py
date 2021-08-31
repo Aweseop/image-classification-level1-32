11b961b81bfbac0a9244c59faca0cc8bdc212e83
ksyoo <hatahtinggroot@gmail.com>
2021-08-31 16:32:16 +0000

    fix circular dependencies bug

--- lab/ks/ks_dataset.py
+++ lab/ks/ks_dataset.py
@@ -1,5 +1,21 @@
+import multiprocessing
 from typing import Any
+from base_dataset import MaskSKFSplitByProfileDataset
 
 class KSSample():
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         print('hello im kwansik')
+
+class KSAgeDataset(MaskSKFSplitByProfileDataset):
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    def __getitem__(self, index):
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        image_transform = self.transform(image)
+
+        age_label = self.get_age_label(index)
+
+        return image_transform, age_label
--- lab/ks/ks_model.py
+++ lab/ks/ks_model.py
@@ -1,5 +1,19 @@
 from typing import Any
+import torch
+import torch.nn as nn
+import torchvision
+import numpy as np
 
-class KSSample():
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        print('hello im kwansik')
+# Custom Model Template
+class KSResnetModel(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
+        self.num_classes = num_classes
+        self.net = torchvision.models.resnet50(pretrained=True)
+        self.net.fc = nn.Linear(in_features=2048, out_features=self.num_classes, bias=True)
+        nn.init.xavier_uniform_(self.net.fc.weight)
+        stdv = 1/np.sqrt(2048)
+        self.net.fc.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        return self.net(x)
