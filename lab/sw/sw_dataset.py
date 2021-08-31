3cef61beb29ac2cfb182208ba44124503d5a02b8
ukcastle <jo631@naver.com>
2021-08-31 16:51:28 +0000

    feat: add sw's custom dataset

--- lab/sw/sw_dataset.py
+++ lab/sw/sw_dataset.py
@@ -1,5 +1,50 @@
 from typing import Any
 
+from torchvision import transforms
+from torchvision.transforms import *
+import torch
+from PIL import Image
+
 class SWSample():
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         print('hello im sw')
+
+
+class BaseAugmentation:
+    def __init__(self, resize, mean, std, **args):
+        self.transform = transforms.Compose([
+            Resize(resize, Image.BILINEAR),
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+        ])
+
+    def __call__(self, image):
+        return self.transform(image)
+
+class ColorJitterAugmentation:
+    def __init__(self, resize, mean, std, **args):
+        self.transform = transforms.Compose([
+            Resize(resize, Image.BILINEAR),
+            ColorJitter(0.1, 0.1, 0.1, 0.1),    
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+        ])
+
+    def __call__(self, image):
+        return self.transform(image)
+
+class GaussianNoiseAugmentation:
+    def __init__(self, resize, mean, std, **args):
+        self.transform = transforms.Compose([
+            Resize(resize, Image.BILINEAR),  
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+            AddGaussianNoise()
+        ])
+
+    def __call__(self, image):
+        return self.transform(image)
+
+# --------- Custom transform -----------
+
+from ...base_dataset import AddGaussianNoise
