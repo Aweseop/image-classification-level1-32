b19889a2158f2db1b29c4bd6a70effcc4a471fd4
YoonyoungL <yyunglee97@gmail.com>
2021-09-03 09:39:16 +0000

    add custom model in yy_model.py

--- lab/yy/yy_model.py
+++ lab/yy/yy_model.py
@@ -1,5 +1,14 @@
-from typing import Any
+import torch
+import torchvision
 
-class YYSample():
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        print('hello im yy')
+class YYResnet50(torch.nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = torchvision.models.wide_resnet50_2(pretrained=True)
+        self.model.fc = torch.nn.Linear(in_features= 2048, out_features=num_classes, bias=True)
+        torch.nn.init.xavier_uniform_(self.model.fc.weight)
+        stdv = 1/(2048**0.5)
+        self.model.fc.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        return self.model(x)
