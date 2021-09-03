cb01cbe97c5031bcdfe59d9b5f3507aec5e30b4e
YoonyoungL <yyunglee97@gmail.com>
2021-09-03 09:30:41 +0000

    fix import in dataset.py model.py

--- model.py
+++ model.py
@@ -4,4 +4,4 @@ from lab.js.js_model import *
 from lab.ks.ks_model import *
 from lab.sw.sw_model import *
 from lab.th.th_model import *
-from lab.sw.sw_model import *
+from lab.yy.yy_model import *
