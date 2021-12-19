c363a225ed4e40dde022103cabb35ca51f7b2824
han-tae-ho <70119696+han-tae-ho@users.noreply.github.com>
2021-12-19 17:14:04 +0900

    Update th_loss.py

--- lab/th/th_loss.py
+++ lab/th/th_loss.py
@@ -2,7 +2,7 @@ from typing import Any
 
 class THLoss():
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        print('hello im th')
+        print('hello im th.')
 
 th_criterion_entrypoints = {
     'thloss': THLoss
